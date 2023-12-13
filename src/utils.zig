19bdacd25ce9ce17a578f0c0a34d0fb43f9b3b83
Josu Igoa <josuigoa@gmail.com>
2023-12-13 16:30:41 +0100

    utils.look_for_dir gehitua

--- src/utils.zig
+++ src/utils.zig
@@ -18,3 +18,36 @@ pub fn apply_separator(path: string) !string {
     _ = std.mem.replace(u8, path, "/", "\\", buf);
     return buf;
 }
+
+pub fn look_for_dir(base_path: string, dir_name: string) !string {
+    const base_dir = try std.fs.openDirAbsolute(base_path, std.fs.Dir.OpenDirOptions{
+        .access_sub_paths = true,
+        .iterate = true,
+        .no_follow = true,
+    });
+
+    var iter = base_dir.iterate();
+    while (true) {
+        const entry = try iter.next();
+        if (entry == null) {
+            break;
+        }
+
+        switch (entry.?.kind) {
+            .directory => {
+                if (std.mem.eql(u8, entry.?.name, dir_name)) {
+                    const look_for = try concat(&.{ base_path, "/", dir_name });
+                    return look_for;
+                } else {
+                    const val = try look_for_dir(try concat(&.{ base_path, "/", entry.?.name }), dir_name);
+                    if (val.len != 0) {
+                        return val;
+                    }
+                }
+            },
+            else => {},
+        }
+    }
+
+    return "";
+}
