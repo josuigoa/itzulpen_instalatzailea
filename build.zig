7100cba1f0706dc344d95642965d72eb053f6825
Josu Igoa <josuigoa@gmail.com>
2024-06-18 13:05:37 +0200

    update to zig 0.13.0

--- build.zig
+++ build.zig
@@ -24,34 +24,47 @@ pub fn build(b: *std.Build) !void {
         b.installArtifact(ui_artifact);
 
         const ui_module = b.addModule("ui", .{
-            .source_file = .{ .path = "lib/ui.zig" },
+            .root_source_file = .{
+                .src_path = .{
+                    .owner = b,
+                    .sub_path = "lib/ui.zig",
+                },
+            },
         });
 
         const utils_module = b.addModule("utils", .{
-            .source_file = .{
-                .path = "src/utils.zig",
+            .root_source_file = .{
+                .src_path = .{
+                    .owner = b,
+                    .sub_path = "src/utils.zig",
+                },
             },
         });
 
         const exe = b.addExecutable(.{
             .name = "installer",
-            .root_source_file = .{ .path = "src/main_installer.zig" },
+            .root_source_file = .{
+                .src_path = .{
+                    .owner = b,
+                    .sub_path = "src/main_installer.zig",
+                },
+            },
             .target = target,
             .optimize = optimize,
         });
 
         exe.subsystem = std.Target.SubSystem.Windows;
-        exe.addModule("utils", utils_module);
-        exe.addModule("ui", ui_module);
+        exe.root_module.addImport("utils", utils_module);
+        exe.root_module.addImport("ui", ui_module);
         exe.linkLibrary(ui_artifact);
 
         const zigimg_module = b.dependency("zigimg", .{
             .target = target,
             .optimize = optimize,
         });
-        exe.addModule("zigimg", zigimg_module.module("zigimg"));
+        exe.root_module.addImport("zigimg", zigimg_module.module("zigimg"));
 
-        if (target.isDarwin()) {
+        if (target.result.isDarwin()) {
             @import("macos_sdk").addPaths(ui_artifact);
             ui_artifact.linkFramework("AppKit");
             ui_artifact.linkFramework("Foundation");
@@ -68,10 +81,13 @@ pub fn build(b: *std.Build) !void {
             exe.linkFramework("CoreFoundation");
             exe.linkFramework("CFNetwork");
             exe.linkFramework("ImageIO");
-        } else if (target.isWindows()) {
+        } else if (target.result.os.tag == .windows) {
             exe.addWin32ResourceFile(.{
                 .file = .{
-                    .path = "src/windows/resources.rc",
+                    .src_path = .{
+                        .owner = b,
+                        .sub_path = "src/windows/resources.rc",
+                    },
                 },
             });
         }
