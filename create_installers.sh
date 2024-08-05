49007993798eb0a2bd5a037cf5ac15864a2f2b7d
Josu Igoa <josuigoa@gmail.com>
2024-08-05 13:52:12 +0200

    linux instalatzailea oinarrizko PUZentzat

--- create_installers.sh
+++ create_installers.sh
@@ -1,4 +1,4 @@
-zig build -Dinstaller-data=$HOME/git/itzulpenak/$1/data -Doptimize=ReleaseSmall 
+zig build -Dinstaller-data=$HOME/git/itzulpenak/$1/data -Doptimize=ReleaseSmall -Dcpu=baseline
 mv zig-out/bin/installer $HOME/git/itzulpenak/$1/instalatzailea/$1_euskaraz_linux
 zig build -Dinstaller-data=$HOME/git/itzulpenak/$1/data -Dtarget=aarch64-macos -Doptimize=ReleaseSmall 
 mv zig-out/bin/installer $HOME/git/itzulpenak/$1/instalatzailea/$1_euskaraz_macos_m1
