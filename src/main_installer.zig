894666c1c7b96b3f555cc3e326e0836e474cbf12
Josu Igoa <josuigoa@gmail.com>
2024-10-04 10:48:28 +0200

    hizkuntza ordezkatzeko aukera gehitu

--- src/main_installer.zig
+++ src/main_installer.zig
@@ -10,6 +10,8 @@ const data = @import("data/installer.zig");
 pub const App = struct {
     window: *ui.Window,
     install_path_entry: *ui.Entry,
+    select_language: bool,
+    language_combobox: *ui.Combobox,
     install_btn: *ui.Button,
 };
 
@@ -62,13 +64,13 @@ pub fn main() !void {
         // v_box.Append(table.as_control(), .stretch);
     }
 
-    const joko_izena = data.get_game_names()[0];
+    const game_name = data.get_game_names()[0];
 
-    const testua: utils.string = if (std.mem.eql(u8, "", game_path))
+    const installer_text: utils.string = if (std.mem.eql(u8, "", game_path))
         "Ez da instalaziorik aurkitu, bilatu zuk mesedez."
     else
         "Ondorengo bidean aurkitu da instalazioa, egiaztatu zuzena ote den.";
-    const label_text = try utils.concat(&.{ "[", joko_izena, "] jokoa euskaratzeko instalatzailea\n", testua });
+    const label_text = try utils.concat(&.{ "[", game_name, "] jokoa euskaratzeko instalatzailea\n", installer_text });
     const label = try ui.Label.New(@ptrCast(label_text));
     v_box.Append(label.as_control(), .dont_stretch);
 
@@ -82,6 +84,19 @@ pub fn main() !void {
     const find_btn = try ui.Button.New("Bilatu");
     bide_box.Append(find_btn.as_control(), .dont_stretch);
 
+    const language_combobox = try ui.Combobox.New();
+    var select_language = false;
+    if (data.get_languages()) |languages| {
+        select_language = true;
+        const combo_label = try ui.Label.New("Ordezkatzeko hizkuntza");
+        v_box.Append(combo_label.as_control(), .dont_stretch);
+        for (languages) |lang| {
+            if (!std.mem.eql(u8, lang, ""))
+                language_combobox.Append(lang);
+        }
+        v_box.Append(language_combobox.as_control(), .dont_stretch);
+    }
+
     const install_btn = try ui.Button.New("Instalatu");
     v_box.Append(install_btn.as_control(), .dont_stretch);
 
@@ -90,6 +105,8 @@ pub fn main() !void {
     var app = App{
         .window = main_window,
         .install_path_entry = install_path_entry,
+        .select_language = select_language,
+        .language_combobox = language_combobox,
         .install_btn = install_btn,
     };
 
@@ -160,7 +177,15 @@ fn install_translation(_: *ui.Button, app_opt: ?*App) void {
     const app = app_opt orelse @panic("Null userdata pointer");
     std.debug.print("Installing in [{s}]\n", .{app.install_path_entry.Text()});
 
-    if (data.install_translation(std.mem.span(app.install_path_entry.Text()))) |opt_installer_response| {
+    const language_index = @as(usize, @intCast(app.language_combobox.Selected()));
+    // ez bada hizkuntzarik aukeratu, combobox-ak -1 itzuliko du.
+    // Baina usize-era cast egitean, usize-k ezin baitu negatiborik adierazi, zenbaki altu bat ematen du
+    if (app.select_language and language_index > 2) {
+        app.window.MsgBoxError("Aukeratu hizkuntza", "Aukeratu zein hizkuntza ordezkatu nahi duzun, mesedez.");
+        return;
+    }
+
+    if (data.install_translation(std.mem.span(app.install_path_entry.Text()), @as(usize, language_index))) |opt_installer_response| {
         if (opt_installer_response) |installer_response| {
             app.window.MsgBox(installer_response.title, installer_response.body);
         } else {
